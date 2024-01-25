030f2dd585404bd9d1eb3546b5be47152337a5a3
RomanGunger <71374915+RomanGunger@users.noreply.github.com>
2024-01-25 18:12:07 +0300

    solution

---
+++ LabyrinthSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DevelopCApp_Homework_03
+{
+	public class LabyrinthSolver
+	{
+
+        public int HasExit(int startI, int startJ, int[,] l)
+        {
+            int exitCount = 0;
+            bool[,] visited = new bool[l.GetLength(0), l.GetLength(1)];
+
+            FindExit(startI, startJ, l, visited, ref exitCount);
+
+            return exitCount;
+        }
+
+        private void FindExit(int i, int j, int[,] l, bool[,] visited, ref int exitCount)
+        {
+            int n = l.GetLength(0);
+            int m = l.GetLength(1);
+
+            if (i < 0 || i >= n || j < 0 || j >= m || visited[i, j])
+                return;
+
+            visited[i, j] = true;
+
+            if (l[i, j] == 1)
+                return;
+
+            if (l[i,j] == 2)
+            {
+                exitCount++;
+                return;
+            }
+
+            FindExit(i + 1, j, l, visited, ref exitCount);
+            FindExit(i - 1, j, l, visited, ref exitCount);
+            FindExit(i, j + 1, l, visited, ref exitCount);
+            FindExit(i, j - 1, l, visited, ref exitCount);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
-﻿
+﻿using DevelopCApp_Homework_03;
+
 public static class Program
 {
     static void Main(string[] args)
     {
+        int[,] labirynth1 = new int[,]
+{
+{1, 1, 1, 1, 1, 1, 1 },
+{1, 0, 0, 0, 0, 0, 1 },
+{1, 0, 1, 1, 1, 0, 1 },
+{0, 0, 0, 0, 1, 0, 2 },
+{1, 1, 0, 0, 1, 1, 1 },
+{1, 1, 1, 0, 1, 1, 1 },
+{1, 1, 1, 2, 1, 1, 1 }
+};
 
+        int startI = 3;
+        int startJ = 0;
+
+
+        LabyrinthSolver labyrinthSolver = new LabyrinthSolver();
+
+        Console.WriteLine("Number of exits: " + labyrinthSolver.HasExit(startI, startJ, labirynth1));
     }
 }
